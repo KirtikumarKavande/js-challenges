6bcbaf88659beec35d252463fa77532e0e2b6493
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-10-05 15:49:02 +0530

    final change in pagination

--- infinite-scroll/index.js
+++ infinite-scroll/index.js
@@ -1,8 +1,30 @@
-let allData=Array(200).fill("").map((item,index)=>`Item ${item+index+1}`)
+let data = Array(200).fill("").map((i, index) => `item ${index + 1}`)
 
-document.addEventListener('DOMContentLoaded',function(){
-console.log("gllaa")
+document.addEventListener('DOMContentLoaded', function () {
+  fetchData(35)
 })
-function loadPage(items){
+document.addEventListener('scroll',onScroll)
 
+let dataFetchedCount = 0
+let dataContainer = document.getElementById("paginated-data")
+let paginatedData = []
+function fetchData(length) {
+  let currentData = data.slice(dataFetchedCount, dataFetchedCount+length)
+  dataFetchedCount = dataFetchedCount + length
+
+  dataContainer.innerHTML = ""
+  paginatedData = [...paginatedData, ...currentData]
+  for (let i = 0; i < paginatedData.length; i++) {
+    let div = document.createElement("div")
+    div.textContent = paginatedData[i]
+
+    dataContainer.appendChild(div)
+  }
+}
+
+function onScroll(){
+let {scrollTop,clientHeight,scrollHeight}= document.documentElement
+if(clientHeight+scrollTop>=scrollHeight){
+  fetchData(10)
+}
 }
--- pagination/index.js
+++ pagination/index.js
@@ -1,30 +1,64 @@
-let data = Array(200).fill("").map((i, index) => `item ${index + 1}`)
-
 document.addEventListener('DOMContentLoaded', function () {
-  fetchData(35)
+  showButton(0)
+  paginationFn(0)
+
 })
-document.addEventListener('scroll',onScroll)
 
-let dataFetchedCount = 0
-let dataContainer = document.getElementById("paginated-data")
-let paginatedData = []
-function fetchData(length) {
-  let currentData = data.slice(dataFetchedCount, dataFetchedCount+length)
-  dataFetchedCount = dataFetchedCount + length
+let data = Array(200).fill(0).map((_, i) => `Item ${i + 1}`);
 
-  dataContainer.innerHTML = ""
-  paginatedData = [...paginatedData, ...currentData]
-  for (let i = 0; i < paginatedData.length; i++) {
-    let div = document.createElement("div")
-    div.textContent = paginatedData[i]
+let noOfBtnPerPage = Math.floor(data.length / 5)
+let currentBtnClicked = 0
+let btnArray=[]
+let btnList = document.getElementById("btn-list")
 
-    dataContainer.appendChild(div)
+function showButton(currentPage){
+
+  for (let i = currentPage>4?currentPage-4:0; i <(currentPage+4<noOfBtnPerPage?currentPage+4:noOfBtnPerPage); i++) {
+    
+    let btns = document.createElement("button")
+    btns.textContent = i + 1
+    btnList.appendChild(btns)
   }
+
 }
 
-function onScroll(){
-let {scrollTop,clientHeight,scrollHeight}= document.documentElement
-if(clientHeight+scrollTop>=scrollHeight){
-  fetchData(10)
+
+  let listContainer = document.getElementById("paginated-data")
+    function paginationFn(currentBtnClicked) {
+  let slicedData = data.slice(currentBtnClicked * 5, (currentBtnClicked + 1) * 5)
+  slicedData.forEach((item) => { 
+    let eachElement = document.createElement('div')
+    eachElement.textContent = item
+    console.log(eachElement)
+    listContainer.appendChild(eachElement)
+
+  })
 }
-}
+btnList.addEventListener('click', function (e) {
+  listContainer.innerHTML = ''
+  currentBtnClicked = e.target.textContent - 1
+  btnList.innerHTML = ''
+
+  showButton(currentBtnClicked)
+
+  paginationFn(currentBtnClicked)
+    
+})
+
+document.getElementById('start').addEventListener('click', function () {
+  listContainer.innerHTML = ''
+  currentBtnClicked=currentBtnClicked-1
+  btnList.innerHTML = ''
+
+  showButton(currentBtnClicked)
+  paginationFn(currentBtnClicked)
+})
+
+document.getElementById('end').addEventListener('click', function () {
+  listContainer.innerHTML = ''
+  currentBtnClicked=currentBtnClicked+1
+  btnList.innerHTML = ''
+
+  showButton(currentBtnClicked)
+  paginationFn(currentBtnClicked)
+})
